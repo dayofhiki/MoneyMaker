8dda577ea0fa222c0e361faf717c0134b13da72f
Donghun Kang <45989184+dayofhiki@users.noreply.github.com>
2026-09-25 00:03:32 +0900

    Fix request 165 lint error

--- victory-trader/src/victory_trader/causal_entry_executability.py
+++ victory-trader/src/victory_trader/causal_entry_executability.py
@@ -28,10 +28,7 @@ from .extended_history_economic_opportunity import (
     _fit_frozen_attention,
 )
 from .flatfiles import MassiveFlatFilesClient, MassiveFlatFileStore
-from .fresh_validated_attention_economic_opportunity import (
-    CANDIDATE_FEATURES,
-    _build_frozen_active,
-)
+from .fresh_validated_attention_economic_opportunity import _build_frozen_active
 from .hierarchical_attention_runtime import add_stage2_scores, fit_stage2
 from .hot_economic_opportunity import ENTRY_FEATURES, _first_hot_feature_rows
 from .market_calendar import is_us_equity_trading_day
